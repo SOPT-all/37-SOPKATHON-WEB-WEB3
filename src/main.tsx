6efe91e8361bc1b0100ab292b3a75a64ceafc6b3
jyeon03 <bjlyeon2003@naver.com>
2025-11-22 22:38:37 +0900

    feat: tanstack-query setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
-import App from "@/App";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from 'react-dom/client';
+import App from '@/App';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ErrorBoundary fallback={<h1>에러 발생</h1>}>
+    <Suspense fallback={<h1>로딩 중..</h1>}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </Suspense>
+  </ErrorBoundary>
 );
