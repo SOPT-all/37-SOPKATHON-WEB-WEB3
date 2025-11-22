9c309aed9a96a3b07c629efc920a3e283d819ca5
jyeon03 <bjlyeon2003@naver.com>
2025-11-23 00:21:32 +0900

    feat: router 설정

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from 'react-router-dom';
 import Layout from '@/router/Layout';
 import { ROUTES } from '@/router/constant/routes';
 import MainPage from '@/pages/main/MainPage';
+import ArchivePage from '@/pages/archive/ArchivePage';
+import ResultPage from '@/pages/results/ResultPage';
+import PostPage from '@/pages/post/PostPage';
 
 const router = createBrowserRouter([
   {
@@ -10,6 +13,18 @@ const router = createBrowserRouter([
       {
         path: ROUTES.HOME,
         element: <MainPage />,
+      },
+      {
+        path: ROUTES.ARCHIVE,
+        element: <ArchivePage />,
+      },
+      {
+        path: ROUTES.RESULTS,
+        element: <ResultPage />,
+      },
+      {
+        path: ROUTES.POST,
+        element: <PostPage />,
       },
     ],
   },
