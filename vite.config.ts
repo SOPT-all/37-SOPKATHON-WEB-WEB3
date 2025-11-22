2115a8c2fd31c4d7085a2c91dee370f6b3147231
jyeon03 <bjlyeon2003@naver.com>
2025-11-23 08:36:31 +0900

    hoxfix: 404 에러

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin(), svgr()],
   resolve: {
     alias: {
-      "@": "/src",
+      '@': '/src',
     },
   },
 });
