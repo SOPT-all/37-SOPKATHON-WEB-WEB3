f66dc4118d1982b996d0bda265f40bac4a6ca991
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2025-11-23 04:11:04 +0900

    style: maxWidth 재설정

--- src/router/Layout.css.ts
+++ src/router/Layout.css.ts
@@ -3,6 +3,7 @@ import { style } from '@vanilla-extract/css';
 export const mainStyle = style({
   margin: '0 auto',
   minHeight: '100dvh',
+  maxWidth: '37.5rem',
   boxShadow: '0 0 1rem rgba(0, 0, 0, 0.1)',
   backgroundColor: '#ffffff',
 });
