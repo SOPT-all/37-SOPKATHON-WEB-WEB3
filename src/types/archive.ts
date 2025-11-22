652adfecb9ed4b2cd8cf8afa790a4feccae49381
jyeon03 <bjlyeon2003@naver.com>
2025-11-23 06:48:08 +0900

    feat: archive 타입 정의

---
+++ src/types/archive.ts
@@ -0,0 +1,38 @@
+import type { FilterType } from '@/pages/archive/constants';
+
+export interface FilterChipsProps {
+  selectedFilter: FilterType;
+  onFilterClick: (filter: FilterType) => void;
+}
+
+export interface DiaryListProps {
+  diaries: Diary[];
+  hasNextPage: boolean;
+  isFetchingNextPage: boolean;
+  observerRef: (node: HTMLDivElement | null) => void;
+}
+
+export interface CardProps {
+  label: 'faith' | 'hope' | 'love';
+  title: string;
+  createdAt: Date;
+}
+
+export interface Diary {
+  diaryId: number;
+  leafType: 'FAITH' | 'HOPE' | 'LOVE';
+  title: string;
+  createdAt: string;
+}
+
+export interface DiariesResponse {
+  diaries: Diary[];
+  nextCursor: number | null;
+  hasNext: boolean;
+}
+
+export interface GetDiariesParams {
+  cursor?: number;
+  size?: number;
+  leafType?: 'FAITH' | 'HOPE' | 'LOVE';
+}
