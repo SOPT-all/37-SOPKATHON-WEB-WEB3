0530be04b23634fcb81b6b4997547767c5981789
huniversal <hunivers0523@naver.com>
2025-11-23 02:25:00 +0900

    feat: 라우터 세팅 진행

--- src/router/Layout.tsx
+++ src/router/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Header from "@/shared/components/header/Header";
 import { mainStyle } from "./Layout.css";
 
 const Layout = () => {
   return (
     <main className={mainStyle}>
+      <Header />
       <Outlet />
     </main>
   );
