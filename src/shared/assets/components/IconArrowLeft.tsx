f68d3b1648953c8b8d94e7e2e8c61772e4bc93ed
huniversal <hunivers0523@naver.com>
2025-11-23 02:24:11 +0900

    feat: 아이콘 컴포넌트화 작업 진행

---
+++ src/shared/assets/components/IconArrowLeft.tsx
@@ -0,0 +1,17 @@
+import type { SVGProps } from 'react';
+const SvgIconArrowLeft = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='1em'
+    height='1em'
+    fill='none'
+    viewBox='0 0 24 24'
+    {...props}
+  >
+    <path
+      fill='#000'
+      d='M9.1 10.73a1.95 1.95 0 0 0 0 2.54l5.33 6.218.053.056a.75.75 0 0 0 1.086-1.032l-5.33-6.22a.45.45 0 0 1 0-.585l5.33-6.219a.75.75 0 0 0-1.139-.976z'
+    />
+  </svg>
+);
+export default SvgIconArrowLeft;
