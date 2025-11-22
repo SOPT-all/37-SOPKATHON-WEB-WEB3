2c9f7a7924f504a238a492d21ad20b12fdf4f6ef
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2025-11-23 04:10:29 +0900

    fix: "" -> '' 수정

--- src/router/Layout.css.ts
+++ src/router/Layout.css.ts
@@ -1,9 +1,8 @@
-import { style } from "@vanilla-extract/css";
+import { style } from '@vanilla-extract/css';
 
 export const mainStyle = style({
-  margin: "0 auto",
-  minHeight: "100dvh",
-  maxWidth: "60rem",
-  boxShadow: "0 0 1rem rgba(0, 0, 0, 0.1)",
-  backgroundColor: "#ffffff",
+  margin: '0 auto',
+  minHeight: '100dvh',
+  boxShadow: '0 0 1rem rgba(0, 0, 0, 0.1)',
+  backgroundColor: '#ffffff',
 });
